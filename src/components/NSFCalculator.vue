c0d3c8a033d15499ed22c7c5f3b7839c31194549
Kamthorn Krairaksa <kamthorn.krairaksa@nectec.or.th>
2025-12-26 17:53:41 +0700

    chore: remove unused onMounted import

--- src/components/NSFCalculator.vue
+++ src/components/NSFCalculator.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, onMounted } from 'vue';
+import { ref, watch } from 'vue';
 import { useNSFCalculation, type CalculationResult } from '../composables/useNSFCalculation';
 
 const { calculateResult } = useNSFCalculation();
